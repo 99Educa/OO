1faa6aff5397666d35aa6b558c3b11d5ea85250a
renatodvm <renatodvm@gmail.com>
2016-09-02 15:39:13 -0300

    Adicionando classes exemplo de baixa e alta coesão.

---
+++ OO/Class/PedidoBaixaCoesao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OO.Class
+{
+    public class PedidoBaixaCoesao
+    {
+        public Cliente Cliente;
+        public bool EhOrcamento;
+
+        public void CalcularDesconto()
+        {
+            var idade = DateTime.Now.Date.Subtract(Cliente.DataNascimento.Date).TotalDays / 365;
+            if (idade < 18)
+            {
+                // Regras para Clientes com menos de 18 anos
+            }
+            else if (idade >= 18 && idade <= 65)
+            {
+                // Regras para Adultos
+            }
+            else
+            {
+                // Regras para Clientes acima de 65 anos
+            }
+        }
+
+        public void AdicionarItem(Produto produto)
+        {
+            // Regras para adicionar um item ao Pedido...
+        }
+
+        public void ConverterEmPedido()
+        {
+            // Regras para converter um orçamento em Pedido...
+        }
+    }
+
+    public class Cliente
+    {
+        public string Nome;
+        public DateTime DataNascimento;
+    }
+
+    public class Produto
+    {
+    }
+}
